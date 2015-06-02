292cc5fe5bc06fedd7f39ee3da16e6cce1f06cc0
niketbp <niketbp@umich.edu>
2015-06-02 10:51:53 -0400

    Update mainViewScript.js

--- js/mainViewScript.js
+++ js/mainViewScript.js
@@ -52,12 +52,15 @@ function displayButtons()
 {
     for(i = 0; i < tasks.length; i++)
     {
-        if (tasks[i].minute>0 && tasks[i].minute <10)
-            date = tasks[i].month+" "+(tasks[i].day)+", "+(tasks[i].year)+" "+(tasks[i].hour)+":0"+(tasks[i].minute)
-        else if (tasks[i].minute===0)
-            date = tasks[i].month+" "+(tasks[i].day)+", "+(tasks[i].year)+" "+(tasks[i].hour)+":"+"00"
-        else
-            date = tasks[i].month+" "+(tasks[i].day)+", "+(tasks[i].year)+" "+(tasks[i].hour)+":"+(tasks[i].minute)
+        if ((tasks[i].minute>0) && (tasks[i].minute <10)){
+            date = tasks[i].month+" "+(tasks[i].day)+", "+(tasks[i].year)+" "+(tasks[i].hour)+":"+"0"+(tasks[i].minute);
+        }
+        else if (tasks[i].minute===0){
+            date = tasks[i].month+" "+(tasks[i].day)+", "+(tasks[i].year)+" "+(tasks[i].hour)+":"+"00";
+        }
+        else{
+            date = tasks[i].month+" "+(tasks[i].day)+", "+(tasks[i].year)+" "+(tasks[i].hour)+":"+(tasks[i].minute);
+        }
         createButton(tasks[i].eventA, getColor(tasks[i].status), tasks[i].index1, date)
     }
 
